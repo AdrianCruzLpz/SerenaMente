b2f933fd4914f099650d2e5d13fcf7368c182bae
adrian <cruzadrian342@gmail.com>
2024-04-21 16:41:21 -0600

    conexion con firebase serenamente

--- login.js
+++ login.js
@@ -1,20 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAnSAUDBaTQQJdcgtu9MFZ2Xpr3oOKNdqw",
-  authDomain: "prueba2-31849.firebaseapp.com",
-  projectId: "prueba2-31849",
-  storageBucket: "prueba2-31849.appspot.com",
-  messagingSenderId: "593735540788",
-  appId: "1:593735540788:web:4fa918ce020f5050c66a61",
+  apiKey: "AIzaSyA0kLe5l_gNuBwhkOOvBr8RO150dHCU31k",
+  authDomain: "serena-mente.firebaseapp.com",
+  projectId: "serena-mente",
+  storageBucket: "serena-mente.appspot.com",
+  messagingSenderId: "183868385167",
+  appId: "1:183868385167:web:442b02f182fc8a28260dfa",
+  measurementId: "G-LVWYEJBRHE"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const db = getFirestore(app);
 
 document.addEventListener("DOMContentLoaded", function () {
   // Seleccionar los elementos del formulario
@@ -38,9 +41,16 @@ document.addEventListener("DOMContentLoaded", function () {
       // Iniciar sesión con Firebase Authentication
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
-
       alert("Inicio de Sesión exitoso");
-      window.location.href = './evaluacionPrevia.html';
+
+      // Verificar si el usuario ha completado la evaluación previa
+      const evaluacionPreviaRealizada = await verificarEvaluacionPrevia();
+
+      if (evaluacionPreviaRealizada) {
+        window.location.href = "./perfil.html";
+      } else {
+        window.location.href = "./evaluacionPrevia.html";
+      }
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -54,7 +64,6 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
     document.getElementById("loginForm").style.display = "none";
     document.getElementById("restorePass").style.display = "block";
-
     const resetPasswordInput = document.getElementById("resetPasswordInput");
     resetPasswordInput.addEventListener("input", function() {
       console.log("El valor ingresado es:", resetPasswordInput.value);
@@ -62,8 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-//Funcion para enviar correo de restablecimiento de contraseña
-
+// Función para enviar correo de restablecimiento de contraseña
 document.addEventListener("DOMContentLoaded", function () {
   const resetPasswordButton = document.getElementById("resetPasswordButton");
   resetPasswordButton.addEventListener("click", (e) => {
@@ -75,7 +83,6 @@ document.addEventListener("DOMContentLoaded", function () {
     try {
       const resetPasswordInput = document.getElementById("resetPasswordInput");
       const email = resetPasswordInput ? resetPasswordInput.value : "";
-
       await sendPasswordResetEmail(auth, email);
       alert("Correo de restablecimiento de contraseña enviado");
     } catch (error) {
@@ -86,3 +93,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+// Función para verificar si el usuario ha completado la evaluación previa
+async function verificarEvaluacionPrevia() {
+  const auth = getAuth();
+  const db = getFirestore(app);
+  const docRef = doc(db, "evaluacionRealizada", auth.currentUser.uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+    return data.evaluacionPreviaRealizada;
+  } else {
+    console.log("No se encontró el documento en evaluacionRealizada");
+    return false;
+  }
+}
